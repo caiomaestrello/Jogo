1640a89e6fd89fac2bcfb048ff7351f91f0609d9
caiomaestrello <105826879+caiomaestrello@users.noreply.github.com>
2022-12-07 18:37:40 -0300

    initial commit

--- var.js
+++ var.js
@@ -1,31 +1,80 @@
-var Jogador1;
-var Jogador2;
+var jogadorEscolha = 0;
+var jogadorPontuacao = 0;
+var computadorEscolha = 0;
+var computadorPontuacao = 0;
+var ganhador = -1;
 
+function jogar(escolha) {
+    jogadorEscolha = escolha;
 
-function Escolha1(){
+    computadorEscolha = Math.floor((Math.random() * (3 - 1 + 1))) + 1;
 
-    Jogador1 = "1";
-}
+    if ((jogadorEscolha) == 1 && (computadorEscolha == 1)) {
+        ganhador = 0;
+    }
 
-function Escolha2(){
+    else if((jogadorEscolha) == 1 && (computadorEscolha == 2)) {
+        ganhador = 2;
+    }
 
-    Jogador1 = "2";
-}
-function Escolha3(){
+    else if((jogadorEscolha) == 1 && (computadorEscolha == 3)) {
+        ganhador = 1;
+    }
 
-    Jogador1 = "3";
-}
+    else if((jogadorEscolha) == 2 && (computadorEscolha == 1)) {
+        ganhador = 1;
+    }
 
-function Escolha1(){
+    else if((jogadorEscolha) == 2 && (computadorEscolha == 2)) {
+        ganhador = 0;
+    }
 
-    Jogador2 = "4";
-}
-function Escolha2(){
+    else if((jogadorEscolha) == 2 && (computadorEscolha == 3)) {
+        ganhador = 2;
+    }
 
-    Jogador2 = "5"; 
-}
+    else if((jogadorEscolha) == 3 && (computadorEscolha == 1)) {
+        ganhador = 2;
+    }
 
-function Escolha3(){
+    else if((jogadorEscolha) == 3 && (computadorEscolha == 2)) {
+        ganhador = 1;
+    }
 
-    Jogador2 = "6";
+    else if((jogadorEscolha) == 3 && (computadorEscolha == 3)) {
+        ganhador = 0;
+    }
+
+    document.getElementById("jogador-escolha-1").classList.remove('selecionado');
+
+    document.getElementById("jogador-escolha-2").classList.remove('selecionado');
+
+    document.getElementById("jogador-escolha-3").classList.remove('selecionado');
+
+    document.getElementById("computador-escolha-1").classList.remove('selecionado');
+
+    document.getElementById("computador-escolha-2").classList.remove('selecionado');
+
+    document.getElementById("computador-escolha-3").classList.remove('selecionado');
+
+    document.getElementById("jogador-escolha-" + jogadorEscolha).classList.add('selecionado');
+    document.getElementById("computador-escolha-" + computadorEscolha).classList.add('selecionado');
+
+    if(ganhador == 0) {
+        document.getElementById('mensagens').innerHTML = 'Empate';
+    }
+
+    else if(ganhador == 1) {
+        document.getElementById('mensagens').innerHTML = 'jogador ganhou';
+        jogadorPontuacao++;
+    }
+
+    else if(ganhador == 2) {
+        document.getElementById('mensagens').innerHTML = 'computador ganhou';
+        computadorPontuacao++;
+    }
+
+    document.getElementById('jogador-pontos').innerHTML = jogadorPontuacao
+    document.getElementById('computador-pontos').innerHTML = computadorPontuacao
+
 }
